fe50e0ddfdd99fd68246686cb51f3c0730a8d1d9
lamiasristy <lamiasristy@gmail.com>
2020-10-29 10:58:55 +0100

    Created the cotroller for User and the views named new and show.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
+    def create
+       @user = User.create(user_params)
+     
+       redirect_to user_path(@user)
+    #    if @user.save
+    #     redirect_to user_path(@user)
+    #    else 
+    #     render :new
+    #    end
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email)
+    end
+
+end
